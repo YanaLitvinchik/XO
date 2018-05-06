d7a48bdc61262098821a22d334747a1ebf80c62a
Yana <Yana@DESKTOP-G09V093>
2018-05-06 18:52:16 +0300

    zz

--- CandX/MyOX.cs
+++ CandX/MyOX.cs
@@ -12,27 +12,32 @@ namespace CandX
         {
             Console.Clear();
             Console.Write(
-$@"{arr[0, 0]}|{arr[1,0]}|{arr[2, 0]}
+$@"{arr[0, 0]}|{arr[1, 0]}|{arr[2, 0]}
 -+-+- 
-{arr[0, 1]}|{arr[1, 1]}|{arr[2,1]} 
+{arr[0, 1]}|{arr[1, 1]}|{arr[2, 1]} 
 -+-+-
 {arr[0, 2]}|{arr[1, 2]}|{arr[2, 2]}
 ");
             Console.SetCursorPosition(X, Y);
         }
-        string[,] arr = new string[,] { {" "," "," " }, { " ", " ", " " }, { " ", " ", " " } };
+        string[,] arr = new string[,] { { " ", " ", " " }, { " ", " ", " " }, { " ", " ", " " } };
         public int X { get; set; }
         public int Y { get; set; }
         public bool ISx { get; set; }
-
-
+        public bool IsWin { get; set; }// false
+        public bool Info()
+        {
+            if (arr[0, 1] == "X" && arr[0, 1] == "X" && arr[0, 2] == "X")
+                IsWin = true;
+            return IsWin;
+        }
         public void Move(ConsoleKey key)
         {
             switch (key)
             {
                 case ConsoleKey.RightArrow:
                     if (X != 4)
-                        X+=2;                    
+                        X += 2;
                     break;
                 case ConsoleKey.LeftArrow:
                     if (X != 0)
@@ -44,36 +49,39 @@ $@"{arr[0, 0]}|{arr[1,0]}|{arr[2, 0]}
                     break;
                 case ConsoleKey.DownArrow:
                     if (Y != 4)
-                        Y+=2;
+                        Y += 2;
                     break;
                 case ConsoleKey.Spacebar:
-                    //0 2 4
-                    //0 1 2
-                    if (ISx && arr[X/2,Y/2] == " ")
-                    {                        
-                        arr[X/2, Y/2] = "O";
+                    if (ISx && arr[X / 2, Y / 2] == " ")
+                    {
+                        arr[X / 2, Y / 2] = "O";
                         ISx = false;
                         break;
                     }
-                    else if(!ISx && arr[X/2, Y/2] == " ")
-                    {                       
-                        arr[X/2, Y/2] = "X";
+                    else if (!ISx && arr[X / 2, Y / 2] == " ")
+                    {
+                        arr[X / 2, Y / 2] = "X";
                         ISx = true;
                         break;
                     }
-                        break;
+                    break;
 
             }
         }
-        
+
         public void Engine()
         {
-            ConsoleKey key;            
+            ConsoleKey key;
             Menu();
             do
             {
-                key = Console.ReadKey().Key;
+                key = Console.ReadKey().Key; //zz
                 Move(key);
+                if (Info() == true)
+                {
+                    Console.WriteLine("you won");
+                    break;
+                }
                 Menu();
             } while (key != ConsoleKey.Enter);
         }
